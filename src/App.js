eb603cebfb2e33deb47cb18e8b17e4cb58417f5a
Roman Lavrenov <rlavrenov@castlighthealth.com>
2019-10-29 15:42:29 -0700

    Added a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Roman', age: 28},
+      {name: 'Kosha', age: 26},
+      {name: 'Nika', age: 4}
+    ]
+  }
   render() {
     return (
       <div className='App'>
         <h1>Privet I am React App</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby is Soccer</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     )
   }
